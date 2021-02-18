36e00b4800a9f0ca38ff8377ca98ea5e9299583d
Masam Mahmood <masam.mahmood@tekhnelogos.com>
2021-02-18 10:18:24 +0300

    Initial Commit

---
+++ autoHeightTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoHeightTableView
+//
+//  Created by Masam Mahmood on 18.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
